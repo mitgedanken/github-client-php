191c799d3125d9b33968204557f54df0b9ae3119
Restyled.io <commits@restyled.io>
2020-08-27 15:44:26 +0000

    Restyled by php-cs-fixer

--- client/objects/GitHubTreeExtra.php
+++ client/objects/GitHubTreeExtra.php
@@ -3,60 +3,58 @@
 require_once(__DIR__ . '/../GitHubObject.php');
 require_once(__DIR__ . '/../objects/GitHubTree.php');
 
-	
+    
 
 class GitHubTreeExtra extends GitHubObject
 {
-	/* (non-PHPdoc)
-	 * @see GitHubObject::getAttributes()
-	 */
-	protected function getAttributes()
-	{
-		return array_merge(parent::getAttributes(), array(
-			'sha' => 'string',
-			'url' => 'string',
-			'tree' => 'array<GitHubTree>',
-		));
-	}
-	
-	/**
-	 * @var string
-	 */
-	protected $sha;
+    /* (non-PHPdoc)
+     * @see GitHubObject::getAttributes()
+     */
+    protected function getAttributes()
+    {
+        return array_merge(parent::getAttributes(), array(
+            'sha' => 'string',
+            'url' => 'string',
+            'tree' => 'array<GitHubTree>',
+        ));
+    }
+    
+    /**
+     * @var string
+     */
+    protected $sha;
 
-	/**
-	 * @var string
-	 */
-	protected $url;
+    /**
+     * @var string
+     */
+    protected $url;
 
-	/**
-	 * @var GitHubTree
-	 */
-	protected $tree;
+    /**
+     * @var GitHubTree
+     */
+    protected $tree;
 
-	/**
-	 * @return string
-	 */
-	public function getSha()
-	{
-		return $this->sha;
-	}
+    /**
+     * @return string
+     */
+    public function getSha()
+    {
+        return $this->sha;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getUrl()
-	{
-		return $this->url;
-	}
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
 
-	/**
-	 * @return GitHubTree
-	 */
-	public function getTree()
-	{
-		return $this->tree;
-	}
-
+    /**
+     * @return GitHubTree
+     */
+    public function getTree()
+    {
+        return $this->tree;
+    }
 }
-
